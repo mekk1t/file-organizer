34a183f52b979329bd8cfe24aeec3d06748e2cc8
mekk1t <kit.kalimov@gmail.com>
2021-07-02 22:08:11 +0300

    Пустой проект консольного приложения

---
+++ FileOrganizer/FileOrganizer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileOrganizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
